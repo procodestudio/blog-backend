a5efccf50608354bca8b3d1ebb7b2a62980eae53
Sergio Sampaio <sergiosampaio@Sergios-MacBook-Pro.local>
2020-05-17 13:59:24 -0300

    Refactored app structure

--- jest.config.js
+++ jest.config.js
@@ -119,7 +119,7 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    './__tests__',
+    './app/__tests__',
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
